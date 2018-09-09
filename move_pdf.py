a97715b0950795e10d9f73e572ab4f4b8e896745
ianmcculloch <ianmcculloch@users.noreply.github.com>
2018-09-09 12:47:08 +0200

    Removed try/catch

--- move_pdf.py
+++ move_pdf.py
@@ -1,13 +1,10 @@
+#Moves pdf files from a source folder to a destination folder
+
 import os, shutil, glob
 
 src_fldr = '/storage/emulated/0/Download'; ## Edit this
 
 dst_fldr = '/storage/emulated/0/Documents'; ## Edit this
-
-#try:
-#  os.makedirs(dst_fldr); ## it creates the destination folder
-#except:
-#  print "Folder already exist or some error";
   
 for pdf_file in glob.glob(src_fldr+"/*.pdf"):
   shutil.move(pdf_file, dst_fldr);
